f29aa015faf08a48cac4c137946e57ddc7e03ce0
taylor4287 <taylor4287.14@gmail.com>
2023-01-17 21:16:51 -0700

    updated db.config and user_models

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,5 +1,16 @@
 const dotenv = require('dotenv');
-
-const Sequelize = require('sequelize');
-
 dotenv.config();
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(process.env.DB_USER, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: env.host,
+    dialect: env.dialect,
+    operatorsAliases: false,
+   
+    pool: {
+      max: env.max,
+      min: env.pool.min,
+      acquire: env.pool.acquire,
+      idle: env.pool.idle
+    }
+});
+
