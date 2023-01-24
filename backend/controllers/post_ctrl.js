1f62cafed1ee244863f7c03dcdc9713abebc981f
taylor4287 <taylor4287.14@gmail.com>
2023-01-24 10:47:10 -0700

    Added middleware; have not added auth to any routes; Added multer_config.js for images

--- backend/controllers/post_ctrl.js
+++ backend/controllers/post_ctrl.js
@@ -1,11 +1,20 @@
-const post = require('../models/post_model');
+const Post = require('../models/post_model');
 const user = require('../models/user_model');
 const fs = require('fs');
-const { runInNewContext } = require('vm');
 
 // get all posts
 exports.allPosts = (req, res, next) => {
-
+    Post.find().then(
+        (posts) => {
+          res.status(200).json(posts);
+        }
+      ).catch(
+        (error) => {
+          res.status(400).json({
+            error: error
+          });
+        }
+    );
 };
 
 // create post
@@ -13,15 +22,12 @@ exports.createPost = (req, res, next) => {
     req.body.post = JSON.parse(req.body.post);
 
     const sauce = new Sauce({
-        authorUsername: req.body.authorUsername,
+        userName: req.body.userName,
+        images: url + '/images/' + req.file.filename,
         date: req.body.date,
+        title: req.body.post.title,
         message: req.body.message,
-        media: req.body.media,
-        likes: 0,
-        dislikes: 0,
-        usersLiked: [],
-        usersDislikes: [],
-        comments: req.body.comments
+        usersRead: req.body.post.usersRead
     });
     post.save().then(
         () => {
@@ -55,16 +61,70 @@ exports.getOne = (req, res, next) => {
 
 // modify post
 exports.modifyPost = (req, res, next) => {
-//     let post = new Post ({ id: req.params.id });
-
-//     Post.findOne({ id: req.params.id }).then(
-//         (post) => {
-//             if
-//         }
-//     )
+    let post = new Post({ _id: req.params._id });
+    Post.findOne({ _id: req.params._id }).then(
+      (post) => {
+        if (req.file) {
+          const url = req.protocol + '://' + req.get('host');
+          req.body.post = JSON.parse(req.body.post);
+          post = {
+            id: req.params.id,
+            userName: req.body.post.userName,
+            images: url + '/images/' + req.file.filename,
+            date: req.body.post.date,
+            title: req.body.post.title,
+            message: req.body.post.message,
+            usersRead: req.body.post.usersRead
+          };
+        } else {
+          post = {
+            id: req.params.id,
+            userName: req.body.userName,
+            images: req.body.images,
+            date: req.body.date,
+            title: req.body.title,
+            message: req.body.message,
+            usersRead: req.body.usersRead
+          };
+        }
+        Post.updateOne({_id: req.params.id}, post).then(
+          () => {
+            res.status(201).json({
+              message: 'Post updated successfully!'
+            });
+          }
+        ).catch(
+          (error) => {
+            console.log(error);
+            res.status(400).json({
+              error: error
+            });
+          }
+        );
+      }
+    )
 };
 
 // delete post
 exports.deletePost = (req, res, next) => {
-
+    Post.findOne({_id: req.params.id}).then(
+        (post) => {
+          const filename = sauce.imageUrl.split('/images/')[1];
+          fs.unlink('images/' + filename, () => {
+            Post.deleteOne({_id: req.params.id}).then(
+              () => {
+                res.status(200).json({
+                  message: 'Deleted!'
+                });
+              }
+            ).catch(
+              (error) => {
+                res.status(400).json({
+                  error: error
+                });
+              }
+            );
+          });
+        }
+      );
 };
--- backend/routers/post_route.js
+++ backend/routers/post_route.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('../middleware/multer_config');
+const auth = require('../middleware/auth');
 
 const postCtrl = require('../controllers/post_ctrl');
 
 router.get('/', postCtrl.allPosts);
 router.get('/', postCtrl.getOne);
-router.post('/', postCtrl.createPost);
-router.put('/', postCtrl.modifyPost);
+router.post('/', multer, postCtrl.createPost);
+router.put('/', multer, postCtrl.modifyPost);
 router.delete('/', postCtrl.deletePost);
 
 module.exports = router;
